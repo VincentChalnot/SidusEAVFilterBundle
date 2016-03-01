227155e0702d6a7c675bbe443ccfb1ed0dff6915
Vincent Chalnot <vchalnot@clever-age.com>
2016-03-01 17:27:18 +0100

    Refactoring EAV references system

--- Filter/Filter.php
+++ Filter/Filter.php
@@ -14,6 +14,12 @@ class Filter extends BaseFilter
     /** @var AttributeInterface[] */
     protected $attributeJoins = [];
 
+    /** @var array */
+    protected $eavReferences = [];
+
+    /** @var bool */
+    protected $isBuilt = false;
+
     /**
      * @param FormInterface $form
      * @param QueryBuilder $qb
@@ -21,10 +27,58 @@ class Filter extends BaseFilter
      */
     public function handleForm(FormInterface $form, QueryBuilder $qb, $alias)
     {
-        $this->getFilterType()->handleForm($this, $form, $qb, $alias);
+        $this->buildAttributeJoins($qb, $alias);
+        parent::handleForm($form, $qb, $alias);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getFormOptions(QueryBuilder $qb, $alias)
+    {
+        $this->buildAttributeJoins($qb, $alias);
+        return parent::getFormOptions($qb, $alias);
+    }
+
+
+    /**
+     * @param QueryBuilder $qb
+     * @param string $alias
+     */
+    protected function buildAttributeJoins(QueryBuilder $qb, $alias)
+    {
+        $this->buildEAVReferences();
         foreach ($this->attributeJoins as $customAlias => $attribute) {
+            if ($this->joinExists($qb, $customAlias)) {
+                continue;
+            }
             $qb->leftJoin($alias . '.values', $customAlias, Join::WITH, "({$customAlias}.attributeCode = '{$attribute->getCode()}')");
         }
+    }
+
+    /**
+     * Build necessary attribute's references for EAV filtering
+     */
+    protected function buildEAVReferences()
+    {
+        if ($this->isBuilt) {
+            return;
+        }
+        foreach ($this->getAttributes() as $attribute) {
+            if (false !== strpos($attribute, '.')) {
+                continue;
+            }
+            if (!empty($this->options['family']) && $this->options['family'] instanceof FamilyInterface) {
+                /** @var FamilyInterface $family */
+                $family = $this->options['family'];
+                if ($family->hasAttribute($attribute)) {
+                    $customAlias = uniqid('join');
+                    $this->eavReferences[$attribute] = $customAlias . '.' . $family->getAttribute($attribute)->getType()->getDatabaseType();
+                    $this->attributeJoins[$customAlias] = $family->getAttribute($attribute);
+                }
+            }
+        }
+        $this->isBuilt = true;
     }
 
     /**
@@ -36,21 +90,33 @@ class Filter extends BaseFilter
         $references = [];
         foreach ($this->getAttributes() as $attribute) {
             if (false === strpos($attribute, '.')) {
-                if (!empty($this->options['family']) && $this->options['family'] instanceof FamilyInterface) {
-                    /** @var FamilyInterface $family */
-                    $family = $this->options['family'];
-                    if ($family->hasAttribute($attribute)) {
-                        $customAlias = uniqid('join');
-                        $references[] = $customAlias . '.' . $family->getAttribute($attribute)->getType()->getDatabaseType();
-                        $this->attributeJoins[$customAlias] = $family->getAttribute($attribute);
-                        continue;
-                    }
+                if (empty($this->eavReferences[$attribute])) {
+                    $references[] = $alias . '.' . $attribute;
+                } else {
+                    $references[] = $this->eavReferences[$attribute];
                 }
-                $references[] = $alias . '.' . $attribute;
             } else {
                 $references[] = $attribute;
             }
         }
         return $references;
+    }
+
+    /**
+     * @param QueryBuilder $qb
+     * @param string $customAlias
+     * @return bool
+     */
+    protected function joinExists(QueryBuilder $qb, $customAlias)
+    {
+        /* @var $join Join */
+        foreach ($qb->getDQLPart('join') as $joins) {
+            foreach ($joins as $join) {
+                if ($join->getAlias() === $customAlias) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
