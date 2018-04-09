24e1f17cf16365b248b9061bd009f489c1cd11db
Vincent Chalnot <vchalnot@clever-age.com>
2018-04-09 18:02:52 +0200

    Switching dependency to BaseBundle

--- DependencyInjection/SidusEAVFilterExtension.php
+++ DependencyInjection/SidusEAVFilterExtension.php
@@ -2,26 +2,13 @@
 
 namespace Sidus\EAVFilterBundle\DependencyInjection;
 
-use Sidus\FilterBundle\DependencyInjection\Loader\ServiceLoader;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Exception\BadMethodCallException;
-use Symfony\Component\DependencyInjection\Extension\Extension;
+use Sidus\BaseBundle\DependencyInjection\SidusBaseExtension;
 
 /**
  * This is the class that loads and manages your bundle configuration
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class SidusEAVFilterExtension extends Extension
+class SidusEAVFilterExtension extends SidusBaseExtension
 {
-    /**
-     * {@inheritdoc}
-     * @throws \Exception
-     * @throws BadMethodCallException
-     */
-    public function load(array $configs, ContainerBuilder $container)
-    {
-        $loader = new ServiceLoader(__DIR__.'/../Resources/config/services');
-        $loader->loadFiles($container);
-    }
 }
